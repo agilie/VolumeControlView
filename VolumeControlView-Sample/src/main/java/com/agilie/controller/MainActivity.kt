ec2ace728c86f2a182f60ef78409b591ed4cb578
Eugene Surkov <eugene.surkov@agilie.com>
2017-06-14 11:02:45 +0300

    update dependency

--- VolumeControlView-Sample/src/main/java/com/agilie/controller/MainActivity.kt
+++ VolumeControlView-Sample/src/main/java/com/agilie/controller/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.widget.SeekBar
 import com.agilie.volumecontrol.animation.controller.ControllerImpl
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 
